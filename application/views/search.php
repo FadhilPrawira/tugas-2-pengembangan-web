a0786c4af879ad50365d40a162481d869e732f7e
FADHIL PRAWIRA <fadhilprawira87@gmail.com>
2023-04-08 12:56:44 +0700

    coba autocomplete

--- application/views/search.php
+++ application/views/search.php
@@ -20,7 +20,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<?php echo form_open('perpustakaan/search'); ?>
 				<div class="input-group my-3">
 					<span class="input-group-text" id="basic-addon2">Judul Buku:</span>
-					<input id="keyword" name="keyword" type="search" class="form-control" placeholder="Contoh: Dilan" aria-label="Contoh: Dilan" aria-describedby="button-addon2">
+					<input id="keyword" name="keyword" type="search" class="form-control" placeholder="Contoh: Dilan" aria-label="Contoh: Dilan" aria-describedby="button-addon2" autocomplete="off">
 					<button class="btn btn-outline-secondary btn-warning text-black" type="submit" id="button-addon2">Cari 
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
 							<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
